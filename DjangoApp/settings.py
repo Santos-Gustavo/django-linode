5f003cf9937d8bbe8cce7e8d79fda73f005179b9
Gustavo Santos <gus.gabriel@hotmail.com>
2022-04-26 20:00:30 +0200

    add my_app to installed apps

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['109.74.192.118']
 # Application definition
 
 INSTALLED_APPS = [
+    'my_app.apps.MyAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
