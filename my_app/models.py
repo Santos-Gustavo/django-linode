86ed22dc3622faf9b3ee49deb1bc80ead75125d5
Gustavo Santos <gus.gabriel@hotmail.com>
2022-05-01 14:54:53 +0200

    Data model complete

--- my_app/models.py
+++ my_app/models.py
@@ -2,18 +2,17 @@ from django.db import models
 
 
 class WorkModel(models.Model):
-    # blank=True -> Field is not required
+    # blank=True -> Field is not compulsory
     name = models.CharField(max_length=30)
     profession = models.CharField(max_length=50)
     contact = models.IntegerField()
-    language_french = models.BooleanField(blank=True)
     salary = models.IntegerField(blank=True)
+    language_french = models.BooleanField(blank=True)
     language_english = models.BooleanField(blank=True)
     language_dutch = models.BooleanField(blank=True)
     drivers_license = models.BooleanField(blank=True)
     description = models.TextField(max_length=1000, blank=True)
-    date = models.DateField()
-    # ToDo: add date form was created
+    date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return (
