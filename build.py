58e64881c705fb952cb6fb135c2f5a190d6b98d0
Gustavo Santos <gus.gabriel@hotmail.com>
2022-05-05 19:34:35 +0200

    Allow website to host

--- build.py
+++ build.py
@@ -6,7 +6,7 @@
 # description = "Sistema completo da rede de imigrantes brasileiros na Belgica!"
 # name = 'DjangoApp'  # Falta de criatividade é foda
 # license = 'GNU GPL v3'
-# version = '0.0.1'
+# version = '0.0.2'
 #
 # default_task = ['clean', 'analyze', 'publish']
 #
