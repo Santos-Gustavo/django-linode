45c36f4b9e0ef59788154d53dff581599cfef316
Gustavo Santos <gus.gabriel@hotmail.com>
2022-05-08 16:58:02 +0200

    add venv and secret_key back top settings.py

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-wtu8(bbv0)h2u#cfzvgguq7qs^mpa)$f2il2+i9&il!(v+#b*8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
