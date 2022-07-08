18378cfe33c7ea67691c3c4d0d1549aae8984077
Gustavo Santos <gus.gabriel@hotmail.com>
2022-07-08 13:22:10 +0200

    adjust model.py

--- my_app/models.py
+++ my_app/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.core.validators import MaxValueValidator
-from django.core.validators import RegexValidator
-from django.urls import reverse
 
 
 class WorkModel(models.Model):
@@ -9,8 +7,7 @@ class WorkModel(models.Model):
     type_job = models.IntegerField()  # 1:Construction, 2:Cleaning, 3:Other
     name = models.CharField(max_length=30)
     profession = models.CharField(max_length=50)
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$')
-    contact = models.CharField(validators=[phone_regex], max_length=17)
+    contact = models.CharField(max_length=17)
     salary = models.FloatField(validators=[MaxValueValidator(99.99)], blank=True)
     language_french = models.BooleanField(blank=True)
     language_english = models.BooleanField(blank=True)
@@ -33,8 +30,7 @@ class ServiceModel(models.Model):
     # blank=True -> Field is not compulsory
     service_type = models.IntegerField()  # 1:Domestic, 2:Healthy and Beauty, 3:Transport
     title = models.CharField(max_length=70)
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$')
-    contact = models.CharField(validators=[phone_regex], max_length=17)
+    contact = models.CharField(max_length=17)
     price = models.FloatField(blank=True)
     image = models.ImageField(upload_to='service/%Y/%m/%d/', blank=True)
     description = models.TextField(max_length=1000, blank=True)
