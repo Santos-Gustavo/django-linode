a26f5d2e8f4d114173667c75348fed86db738f42
root <gus.gabriel@hotmail.com>
2022-07-15 18:18:47 +0000

    changes from prod

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'DjangoApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('DATABASE_ENGINE'),
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
     }
 }
 
